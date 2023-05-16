d8331c83232d801fc954b5502f03105ea657be01
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-05-16 14:08:32 +0700

    update

--- client.py
+++ client.py
@@ -3,20 +3,6 @@ import socketio
 
 sio = socketio.Client()
 
-@sio.on('update')
-def handle_update(data):
-    unrevealed_word = data['unrevealed_word']
-    is_correct = data['is_correct']
-    score = data['score']
-
-    print(f'Unrevealed word: {unrevealed_word}')
-    if is_correct:
-        print(f'Your guess is correct! Score: {score}')
-    else:
-        print('Your guess is incorrect.')
-
-    print('------------------------------')
-
 @sio.on('connect')
 def handle_connect():
     print('Connected to the server')
@@ -28,14 +14,3 @@ def handle_disconnect():
     print('Disconnected from the server')
 
 sio.connect('http://localhost:5000')
-
-while True:
-    guess = input('Enter your guess (or "ready" to start): ')
-
-    sio.emit('guess', {'guess': guess})
-
-    if guess.lower() == 'ready':
-        print('Waiting for the game to start...')
-        print('------------------------------')
-
-sio.disconnect()
--- server.py
+++ server.py
@@ -9,6 +9,7 @@ socketio = SocketIO(app)
 player_scores = {}
 word = ""
 unrevealed_word = ""
+player_name = ""
 
 def load_words():
     with open("word_list.json") as f:
@@ -19,7 +20,7 @@ def select_word(words):
     return random.choice(words)
 
 def generate_unrevealed_word(word_slice):
-    word = word_slice['word_entry']
+    word = word_slice["word_entry"]
     return "".join(["-" if c != " " else " " for c in word])
 
 def handle_guess(guess, word, unrevealed_word):
@@ -30,67 +31,84 @@ def handle_guess(guess, word, unrevealed_word):
     if guess_lower in word_lower:
         for i, c in enumerate(word_lower):
             if c == guess_lower:
-                new_unrevealed_word = new_unrevealed_word[:i] + word[i] + new_unrevealed_word[i + 1 :]
+                new_unrevealed_word = (
+                    new_unrevealed_word[:i] + word[i] + new_unrevealed_word[i + 1 :]
+                )
         return new_unrevealed_word, True
     else:
         return new_unrevealed_word, False
 
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
 
+@app.route("/start_game")
+def start_game_route():
+    return render_template("game.html", player_name=player_name)
 
-@app.route("/")
-def game_page():
-    return render_template("game.html")
+@socketio.on("start_game")
+def start_game(data):
+    global player_name
+    player_name = data["player_name"]
+    socketio.emit("redirect", "/start_game")
 
 @socketio.on("connect")
 def handle_connect():
-    global player_id
-    player_id = request.sid
-    player_scores[player_id] = 0
+    player_scores[request.sid] = 0
     print("A player connected")
-    
-    # Send initial game state to the connected player
+
     emit(
         "update",
         {
             "unrevealed_word": unrevealed_word,
             "is_correct": None,
-            "score": None,
-            "player_id": player_id,
+            "score": player_scores[request.sid],
+            "desc": word["description"],
+            "player_name": player_name,
         },
-        room=player_id,
+        room=request.sid,
     )
+
 
 @socketio.on("guess")
 def handle_guess_event(data):
     global word
     global unrevealed_word
-    
+
     guess = data["guess"].strip().lower()
     if guess == "ready":
         print("Player is ready")
         return
 
-    unrevealed_word, is_correct = handle_guess(guess, word['word_entry'], unrevealed_word)
+    unrevealed_word, is_correct = handle_guess(
+        guess, word["word_entry"], unrevealed_word
+    )
 
     score_multiplier = 0
     if is_correct:
-        # Calculate the score
-        score_multiplier = 100 * word['word_entry'].count(guess)
-        player_scores[player_id] += score_multiplier
+        score_multiplier = 100 * word["word_entry"].lower().count(guess)
+        player_scores[request.sid] += score_multiplier
+
+    if not is_correct:
+        socketio.emit("wrong", room=request.sid)
 
     if "-" not in unrevealed_word:
         print(f"Word revealed: {word}")
-        # Handle game completion here
+        socketio.emit("win", room=request.sid)
 
-    # Send the updated game state to all players
     emit(
         "update",
         {
             "unrevealed_word": unrevealed_word,
             "is_correct": is_correct,
-            "score": score_multiplier,
+            "score": player_scores[request.sid],
+            "desc": word["description"],
+            "player_name": player_name,
         },
     )
+    print("score", player_scores)
+    print(player_scores[request.sid])
+
 
 if __name__ == "__main__":
     words = load_words()
