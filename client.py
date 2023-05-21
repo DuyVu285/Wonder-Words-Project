8ad186b1552d63829041589add1906f4032a5e3f
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-05-21 15:36:17 +0700

    update

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 import webbrowser
 import socketio
+import subprocess
 
 sio = socketio.Client()
+
+# Store the web browser process
+browser_process = None
 
 
 @sio.on("connect")
 def handle_connect():
     print("Connected to the server")
     # Open home.html in a web browser
-    webbrowser.open("http://localhost:5000")
+    global browser_process
+    browser_process = subprocess.Popen(["webbrowser", "http://localhost:5000"])
 
 
 @sio.on("disconnect")
 def handle_disconnect():
     print("Disconnected from the server")
-    return
+    # Close the web browser
+    global browser_process
+    if browser_process:
+        browser_process.kill()
 
 
 sio.connect("http://localhost:5000")
